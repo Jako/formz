a69d5e61c5ef03177830cc5dce001d8cb843a757
vierkantemeter <mark@vierkante-meter.nl>
2014-09-08 13:35:54 +0200

    Added Dutch (nl) lexicon properties.inc.php

---
+++ core/components/formz/lexicon/nl/properties.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * formz
+ *
+ * @package formz
+ */
+/**
+ * Properties Dutch Lexicon Entries for formz
+ *
+ * @package formz
+ * @subpackage lexicon
+ */
+$_lang['prop_formz.tpl'] = 'Standaard formulier template';
+$_lang['prop_formz.id'] = 'Formulier ID';
+$_lang['prop_formz.field_tpl'] = 'Standaard formulierveld template';
